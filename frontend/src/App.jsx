9d91f085e2e21642f95d5f45c6405c2990732ed0
naveen <naveenagrawal1008@gmail.com>
2025-04-27 22:49:33 +0530

    setting routes in app file for routing and setting up axios.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,30 @@
-
 import './App.css'
-
+import { Route,Routes } from 'react-router-dom'
+import { NavBar } from './components/NavBar'
+import { HomePage } from './pages/HomePage'
+import { SignUp } from './pages/SignUp'
+import { LoginPage } from './pages/LoginPage'
+import { SettingPage } from './pages/SettingPage'
+import { ProfilePage } from './pages/ProfilePage'
+import { axiosInstance } from './lib/axios'
 function App() {
+
+  
   
 
   return (
     <>
-      <p className="bg-pink-400">trash</p>
+    <NavBar/>
+      <Routes>
+      <Route path='/' element={<HomePage/>}/>
+      <Route path='/signup' element={<SignUp/>}/>
+      <Route path='/login' element={<LoginPage/>}/>
+      <Route path='/setting' element={<SettingPage/>}/>
+      <Route path='/profile' element={<ProfilePage/>}/>
+
+
+
+      </Routes>
     </>
   )
 }
