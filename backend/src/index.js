c115c1c95d80905dd6936c4c47fe6cd125850342
naveen <naveenagrawal1008@gmail.com>
2025-05-15 14:31:00 +0530

    changes onrender url

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ app.use(cookieparser())
 app.use(cors({
     
         origin: process.env.NODE_ENV === "production" 
-        ? "https://your-render-app-url.onrender.com" 
+        ? "https://chirp-va60.onrender.com" 
         : "http://localhost:5173",
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE"],
@@ -32,10 +32,13 @@ app.use("/api/messages",messageRoutes)
 
 
 if(process.env.NODE_ENV ===   "production"){
-    app.use(express.static(path.join(__dirname,'../frontend/dist')));
+
+    const staticPath = path.join(__dirname,"..", "frontend","dist")
+
+    app.use(express.static(staticPath));
 
     app.get("*",(req,res)=>{
-        res.sendFile(path.join(__dirname,'../frontend','dist','index.html'));
+        res.sendFile(path.join(staticPath,'index.html'));
     });
 
 }
--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 const io = new Server(server,{
     cors:{
         origin:process.env.NODE_ENV === "production" 
-            ? "https://your-render-app-url.onrender.com" 
+            ? "https://chirp-va60.onrender.com"
             : "http://localhost:5173",
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE"],
